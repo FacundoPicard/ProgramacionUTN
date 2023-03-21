9d1f4bdac05d084dd2959f7cdb3ac3a40c6bd82b
Facundo Picard <facundopicard09@gmail.com>
2023-03-21 17:52:27 -0300

    portafolio

---
+++ PORTAFOLIO/js/index.js
@@ -0,0 +1,20 @@
+/* maquina de escribir */
+
+/* let text= documemt.getElementBy("textoinicio");
+let str= text.innerHTML;
+
+text.innerHTML = "";
+
+let speed= 200;
+let i = 0
+
+function maquinaescribir(){
+    if(i < str.length){
+        text.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(maquinaescribir, speed);
+    }
+}
+
+setTimeout(maquinaescribir, speed);
+ */
