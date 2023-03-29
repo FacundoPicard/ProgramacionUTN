ad1c601c12b68749bfae8774f10b684895a86701
Facundo Picard <facundopicard09@gmail.com>
2023-03-29 13:16:11 -0300

    29/03/2023

--- PORTAFOLIO/js/index.js
+++ PORTAFOLIO/js/index.js
@@ -1,20 +1,4 @@
-/* maquina de escribir */
-
-/* let text= documemt.getElementBy("textoinicio");
-let str= text.innerHTML;
-
-text.innerHTML = "";
-
-let speed= 200;
-let i = 0
-
-function maquinaescribir(){
-    if(i < str.length){
-        text.innerHTML += str.charAt(i);
-        i++;
-        setTimeout(maquinaescribir, speed);
-    }
+function cerrar() {
+  let navbar = document.querySelector(".navbar-toggler");
+  navbar.click();
 }
-
-setTimeout(maquinaescribir, speed);
- */
