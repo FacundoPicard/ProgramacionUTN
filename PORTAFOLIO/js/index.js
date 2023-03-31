e9e8de228de5589eec4934a5d9dbac25b513e949
Facundo Picard <facundopicard09@gmail.com>
2023-03-31 12:08:35 -0300

    s

--- PORTAFOLIO/js/index.js
+++ PORTAFOLIO/js/index.js
@@ -2,3 +2,12 @@ function cerrar() {
   let navbar = document.querySelector(".navbar-toggler");
   navbar.click();
 }
+
+
+function Ingresarmouse(event){
+document.getElementById("facundopicardfooter").style.color = "#8cb3df"
+}
+
+function Salirmouse(event){
+  document.getElementById("facundopicardfooter").style.color = "white"
+  }
