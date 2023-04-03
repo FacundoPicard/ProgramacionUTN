6e0809fffc5fa0334e58c33ea2c0d2803aa19cbf
Facundo Picard <facundopicard09@gmail.com>
2023-04-03 20:02:57 -0300

    03.04.2023

--- PORTAFOLIO/js/index.js
+++ PORTAFOLIO/js/index.js
@@ -3,6 +3,19 @@ function cerrar() {
   navbar.click();
 }
 
+/* var i=0;
+txt= "Hola Soy Facundo Picard Programador Web Junior. Front End Developer."
+var speed=80;
+
+function typeWrite(){
+if(i<txt.length){
+document.getElementById("tituloinicio").innerHTML += txt.charAt(i);
+i++;
+setTimeout(typeWrite, speed);
+}
+}
+typeWrite(); */
+
 
 function Ingresarmouse(event){
 document.getElementById("facundopicardfooter").style.color = "#8cb3df"
