bfbca0ab7f99a8265169cc3dd6fb2bbcf9aa4df0
Facundo Picard <facundopicard09@gmail.com>
2023-04-05 19:24:04 -0300

    05.04.23

--- M3U1/app.js
+++ M3U1/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var contactoRouter = require("./routes/contacto");
 
 var app = express();
 
@@ -20,7 +20,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/contacto", contactoRouter);
+
+
+app.get('/nosotros', function (req,res){
+  res.render("nosotros")
+})
+
+app.get('/servicios', function (req,res){
+  res.render("servicios")
+})
+
+app.get('/contacto', function (req,res){
+  res.render("contacto")
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
