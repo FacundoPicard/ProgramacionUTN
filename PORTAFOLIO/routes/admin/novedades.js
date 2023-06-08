6e60c5628227bd7d837a2f27040d857158e59fb5
Facundo Picard <facundopicard09@gmail.com>
2023-06-08 11:16:03 +0200

    8.6.23

--- PORTAFOLIO/routes/admin/novedades.js
+++ PORTAFOLIO/routes/admin/novedades.js
@@ -1,14 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require("../../models/novedadesModel");
 
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout:'admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre, novedades
     });
   });
 
+  router.get("/eliminar/:id", async (req,res,next) => {
+    const id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect("/admin/novedades")
+  });
+
+  router.get("/agregar", (req, res, next) =>{
+    res.render("admin/agregar", {
+      layout: "admin/layout"
+    });
+  });
+
+  router.post("/agregar", async (req, res, next) => {
+    try {
+      if (req.body.titulo != "" && req.body.subtitulo != "") {
+        await novedadesModel.insertNovedad(req.body);
+        res.redirect("/admin/novedades")
+      } else {
+        res.render("admin/agregar", {
+          layout: "admin/layout",
+          error: true, message: "Todos los campos son requeridos"
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      res.render("admin/agregar", {
+        layout: "admin/layout" ,
+        error: true, message: "No se cargo la habilidad"
+      });
+    }
+  });
 
 
   module.exports = router;
